d8daf1130b2bfc9be2c9e212ba01eb334ea7007a
MATVII <mattor264@gmail.com>
2026-01-08 00:58:01 +0200

    Make extension for getting user id from credentials, add mutation endpoit and payload for adding help request

---
+++ server/Presentation/GraphQL/Extensions/GraphQLExtension.cs
@@ -0,0 +1,40 @@
+﻿using GraphQL;
+using server.Presentation.GraphQL.Helpers;
+using System.Security.Claims;
+
+namespace server.Presentation.GraphQL.Extensions
+{
+    public static class GraphQLExtensions
+    {
+        public static Guid GetUserId(this IResolveFieldContext context)
+        {
+            var userContext = context.UserContext as GraphQLUserContext;
+
+            var claim = userContext?.User?.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim == null)
+                throw new ExecutionError("UNAUTHORIZED");
+
+            return Guid.Parse(claim.Value);
+        }
+
+        public static string GetUserEmail(this IResolveFieldContext context)
+        {
+            var userContext = context.UserContext as GraphQLUserContext;
+
+            var claim = userContext?.User?.FindFirst(ClaimTypes.Email);
+
+            if (claim == null)
+                throw new ExecutionError("EMAIL_CLAIM_MISSING");
+
+            return claim.Value;
+        }
+
+        public static bool IsInRole(this IResolveFieldContext context, string role)
+        {
+            var userContext = context.UserContext as GraphQLUserContext;
+
+            return userContext?.User?.IsInRole(role) ?? false;
+        }
+    }
+}
---
+++ server/Presentation/GraphQL/Types/AddHelpRequestPayloadType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using server.Application.Handlers.AddHelpRequest;
+
+namespace server.Presentation.GraphQL.Types
+{
+    public class AddHelpRequestPayloadType
+    : ObjectGraphType<AddHelpRequestResult>
+    {
+        public AddHelpRequestPayloadType()
+        {
+            Field(x => x.Success);
+            Field(x => x.HelpRequestId, nullable: true);
+            Field(x => x.ErrorCode, nullable: true);
+            Field(x => x.ErrorMessage, nullable: true);
+        }
+    }
+}
