2be96d39484018b73a6d4d16698f37e93a001bd6
MATVII <mattor264@gmail.com>
2026-01-08 00:05:35 +0200

    Add command, result and handler for adding help request

---
+++ server/Application/Handlers/AddHelpRequest/AddHelpRequestCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace server.Application.Handlers.AddHelpRequest
+{
+    public record AddHelpRequestCommand(
+        Guid CreatorId,
+        string Title,
+        string Description,
+        double? Latitude,
+        double? Longitude,
+        IReadOnlyList<string> ImageUrls) : IRequest<AddHelpRequestResult>;
+}
