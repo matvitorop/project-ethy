6a765e44380bfcd201ec066425768f49c104e654
MATVII <mattor264@gmail.com>
2026-01-28 00:38:10 +0200

    Add dto, query and handler for help request get, add new method in appropriate repo

---
+++ server/Application/Handlers/GetHelpRequests/GetHelpRequestsPageHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using server.Application.Handlers.GetActiveRequests;
+using server.Application.IRepositories;
+
+namespace server.Application.Handlers.GetHelpRequests
+{
+    public class GetHelpRequestsPageHandler
+    : IRequestHandler<GetHelpRequestsPageQuery, IReadOnlyList<HelpRequestListItemDto>>
+    {
+        private readonly IHelpRequestRepository _repo;
+
+        public GetHelpRequestsPageHandler(IHelpRequestRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IReadOnlyList<HelpRequestListItemDto>> Handle(
+            GetHelpRequestsPageQuery request,
+            CancellationToken ct)
+        {
+            return await _repo.GetPageAsync(
+                request.Page,
+                request.PageSize,
+                ct);
+        }
+    }
+}
---
+++ server/Application/Handlers/GetHelpRequests/HelpRequestListItemDto.cs
@@ -0,0 +1,12 @@
+﻿namespace server.Application.Handlers.GetActiveRequests
+{
+    public sealed class HelpRequestListItemDto
+    {
+        public Guid Id { get; init; }
+        public string Title { get; init; } = null!;
+        public string Category { get; init; } = null!;
+        public string Status { get; init; } = null!;
+        public string? PreviewImageUrl { get; init; }
+        public DateTime CreatedAt { get; init; }
+    }
+}
