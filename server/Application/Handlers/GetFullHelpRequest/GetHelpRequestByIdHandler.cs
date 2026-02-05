aed2f0e414195ba8b74daed76548709ef1302eaf
MATVII <mattor264@gmail.com>
2026-02-05 15:12:45 +0200

    Add handler for getting detail help request, payload and types for graphql

---
+++ server/Application/Handlers/GetFullHelpRequest/GetHelpRequestByIdHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using server.Application.IRepositories;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.GetFullHelpRequest
+{
+    public sealed class GetHelpRequestByIdHandler : IRequestHandler<GetFullHelpRequestQuery, Result<HelpRequestDetailDto>>
+    {
+        private readonly IHelpRequestRepository _repo;
+
+        public GetHelpRequestByIdHandler(IHelpRequestRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Result<HelpRequestDetailDto>> Handle(
+            GetFullHelpRequestQuery request,
+            CancellationToken ct)
+        {
+            var item = await _repo.GetHelpRequestById(ct, request.HelpRequestId);
+
+            if (item is null)
+            {
+                return Result<HelpRequestDetailDto>.Failure(
+                    new Error(
+                        "Help request not found.",
+                        "HelpRequest.NotFound"
+                    )
+                );
+            }
+
+            return Result<HelpRequestDetailDto>.Success(item);
+        }
+    }
+}
