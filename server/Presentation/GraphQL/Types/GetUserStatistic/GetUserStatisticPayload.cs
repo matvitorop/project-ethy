1296637d34b9b4db9a8ff5b0911b407ae1a047ac
MATVII <mattor264@gmail.com>
2026-02-14 21:43:00 +0200

    Add new fields for user trust system with method for changing user requests limits, add endpoint for getting user statistic with appropriate payload types

---
+++ server/Presentation/GraphQL/Types/GetUserStatistic/GetUserStatisticPayload.cs
@@ -0,0 +1,11 @@
+﻿using server.Application.Handlers.GetUserStatistics;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.GetUserStatistic
+{
+    public sealed record GetUserStatisticPayload(
+        UserStatisticsDto? Data,
+        ErrorPayload? Error
+        );
+    
+}
---
+++ server/Presentation/GraphQL/Types/GetUserStatistic/UserStatisticsGraphType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using server.Application.Handlers.GetUserStatistics;
+
+namespace server.Presentation.GraphQL.Types.GetUserStatistic
+{
+    public sealed class UserStatisticsGraphType : ObjectGraphType<UserStatisticsDto>
+    {
+        public UserStatisticsGraphType()
+        {
+            Name = "UserStatistics";
+
+            Field(x => x.RegisteredAtUtc);
+            Field(x => x.TotalRequests);
+            Field(x => x.ActiveRequests);
+            Field(x => x.CompletedRequests);
+        }
+    }
+}
