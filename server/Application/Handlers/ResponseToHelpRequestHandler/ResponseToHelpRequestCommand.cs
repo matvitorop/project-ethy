39c06025ea63c84a2e11c8b37c8f45dd0c73a163
MATVII <mattor264@gmail.com>
2026-03-17 13:07:19 +0200

    Make new handler layout and command for adding hr response, add new table as migration for hr response, add new error handling and getting id of assigned user method

---
+++ server/Application/Handlers/ResponseToHelpRequestHandler/ResponseToHelpRequestCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.ResponseToHelpRequestHandler
+{
+    public sealed record ResponseToHelpRequestCommand(
+        Guid HelpRequestId, 
+        Guid UserId,
+        string Message) : IRequest<Result>;
+}
