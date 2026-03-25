411af81161a549cb1056151118be64abff1f113d
MATVII <mattor264@gmail.com>
2026-03-25 17:38:25 +0200

    Add full functionality for getiing responses by request owner

---
+++ server/Presentation/GraphQL/Types/GetHRResponsesTypes/HelpRequestResponsesPayload.cs
@@ -0,0 +1,10 @@
+﻿using server.Application.Handlers.GetHelpRequestResponses;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.GetHRResponsesTypes
+{
+    public sealed record HelpRequestResponsesPayload(
+       IReadOnlyList<HelpRequestResponseDto>? Items,
+       ErrorPayload? Error
+   );
+}
