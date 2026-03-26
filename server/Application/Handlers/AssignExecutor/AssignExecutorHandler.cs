867d1082be410de12910bc345f3d86580b3ffa10
MATVII <mattor264@gmail.com>
2026-03-26 13:26:12 +0200

    Create light version of handler, that just assigning executor

---
+++ server/Application/Handlers/AssignExecutor/AssignExecutorHandler.cs
@@ -0,0 +1,52 @@
+﻿using MediatR;
+using server.Application.IRepositories;
+using server.Domain.Exceptions;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.AssignExecutor
+{
+    public sealed class AssignExecutorHandler
+        : IRequestHandler<AssignExecutorCommand, Result<AssignExecutorResult>>
+    {
+        private readonly IHelpRequestRepository _repository;
+
+        public AssignExecutorHandler(IHelpRequestRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<Result<AssignExecutorResult>> Handle(
+            AssignExecutorCommand request,
+            CancellationToken ct)
+        {
+            var helpRequest = await _repository
+                .GetAggregateByIdAsync(ct, request.HelpRequestId);
+
+            if (helpRequest is null)
+                return Result<AssignExecutorResult>.Failure(
+                    new Error("Help request not found", "HelpRequest.NOT_FOUND"));
+
+            if (helpRequest.CreatorId != request.CurrentUserId)
+                return Result<AssignExecutorResult>.Failure(
+                    new Error("Access denied", "HelpRequest.FORBIDDEN"));
+
+            try
+            {
+                helpRequest.AssignExecutor(request.ResponseId);
+            }
+            catch (DomainException ex)
+            {
+                return Result<AssignExecutorResult>.Failure(
+                    new Error(ex.Message, ex.Code));
+            }
+
+            await _repository.UpdateAsync(helpRequest, ct);
+
+            return Result<AssignExecutorResult>.Success(
+                new AssignExecutorResult(
+                    helpRequest.Id,
+                    helpRequest.AssignedUserId!.Value
+                ));
+        }
+    }
+}
