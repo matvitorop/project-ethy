fb8b1f43c17092cbd8b39f69550a7c95e0ddc068
MATVII <mattor264@gmail.com>
2026-03-26 15:04:27 +0200

    Add payload and graphql types for resolver

---
+++ server/Presentation/GraphQL/Types/AssignExecutorTypes/AssignExecutorPayload.cs
@@ -0,0 +1,10 @@
+﻿using server.Application.Handlers.AssignExecutor;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.AssignExecutorTypes
+{
+    public sealed record AssignExecutorPayload(
+        AssignExecutorResult? Data,
+        ErrorPayload? Error
+    );
+}
---
+++ server/Presentation/GraphQL/Types/AssignExecutorTypes/AssignExecutorPayloadType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.AssignExecutorTypes
+{
+    public sealed class AssignExecutorPayloadType
+        : ObjectGraphType<AssignExecutorPayload>
+    {
+        public AssignExecutorPayloadType()
+        {
+            Field<AssignExecutorResultType>("data")
+                .Resolve(ctx => ctx.Source.Data);
+
+            Field<ErrorPayloadType>("error")
+                .Resolve(ctx => ctx.Source.Error);
+        }
+    }
+}
