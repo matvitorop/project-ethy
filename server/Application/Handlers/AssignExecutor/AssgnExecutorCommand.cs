5d37d526f261d18c7dfcca1524024152be0bf2c7
MATVII <mattor264@gmail.com>
2026-03-26 13:12:22 +0200

    Add result and command records for handler

---
+++ server/Application/Handlers/AssignExecutor/AssgnExecutorCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.AssignExecutor
+{
+    public sealed record AssgnExecutorCommand(
+        Guid HelpRequestId,
+        Guid ResponseId,
+        Guid CurrentUserId
+    ) : IRequest<Result<AssignExecutorResult>>;
+}
