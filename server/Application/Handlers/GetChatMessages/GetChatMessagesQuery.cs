03eed91ae91234816591165f3c5faea2efd1d139
MATVII <mattor264@gmail.com>
2026-04-13 13:31:34 +0300

    Add dto for messages, getting messages query and interface for chat repo

---
+++ server/Application/Handlers/GetChatMessages/GetChatMessagesQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.GetChatMessages
+{
+    public sealed record GetChatMessagesQuery(Guid HelpRequestId,
+        Guid RequestingUserId
+    ) : IRequest<Result<IReadOnlyList<ChatMessageDto>>>;
+}
