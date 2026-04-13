bce9fb8f1550a84dd31437adbf1b902cd5903a5c
MATVII <mattor264@gmail.com>
2026-04-13 13:22:52 +0300

    Create classes for chat info and messages

---
+++ server/Domain/Chat/Chat.cs
@@ -0,0 +1,22 @@
+﻿namespace server.Domain.Chat
+{
+    public class Chat
+    {
+        public Guid Id { get; private set; }
+        public Guid HelpRequestId { get; private set; }
+        public Guid OwnerId { get; private set; }
+        public Guid AssigneeId { get; private set; }
+        public DateTime CreatedAtUtc { get; private set; }
+
+        private Chat() { }
+
+        public Chat(Guid helpRequestId, Guid ownerId, Guid assigneeId)
+        {
+            Id = Guid.NewGuid();
+            HelpRequestId = helpRequestId;
+            OwnerId = ownerId;
+            AssigneeId = assigneeId;
+            CreatedAtUtc = DateTime.UtcNow;
+        }
+    }
+}
---
+++ server/Domain/Chat/ChatMessage.cs
@@ -0,0 +1,34 @@
+﻿using server.Domain.Exceptions;
+
+namespace server.Domain.Chat
+{
+    public class ChatMessage
+    {
+        public Guid Id { get; private set; }
+        public Guid ChatId { get; private set; }
+        public Guid SenderId { get; private set; }
+        public string Content { get; private set; }
+        public DateTime CreatedAtUtc { get; private set; }
+
+        private ChatMessage() { }
+
+        public ChatMessage(Guid chatId, Guid senderId, string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+                throw new DomainException(
+                    "Message content is required",
+                    "ChatMessage.CONTENT_REQUIRED");
+
+            if (content.Length > 2000)
+                throw new DomainException(
+                    "Message is too long",
+                    "ChatMessage.CONTENT_TOO_LONG");
+
+            Id = Guid.NewGuid();
+            ChatId = chatId;
+            SenderId = senderId;
+            Content = content;
+            CreatedAtUtc = DateTime.UtcNow;
+        }
+    }
+}
