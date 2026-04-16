c1f37031bcb5e67d113ce5f83218e1c7d4e5e284
MATVII <mattor264@gmail.com>
2026-04-16 21:20:57 +0300

    Create handler for getting stages from template template

---
+++ server/Application/Handlers/GetStageTemplates/GetStageTemplatesQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.GetStageTemplates
+{
+    public sealed record GetStageTemplatesQuery
+        : IRequest<Result<IReadOnlyList<StageTemplateDto>>>;
+}
---
+++ server/Application/Handlers/GetStageTemplates/StageTemplateDto.cs
@@ -0,0 +1,8 @@
+﻿namespace server.Application.Handlers.GetStageTemplates
+{
+    public sealed record StageTemplateDto(
+        Guid Id,
+        string Content,
+        bool IsAutomatic
+    );
+}
