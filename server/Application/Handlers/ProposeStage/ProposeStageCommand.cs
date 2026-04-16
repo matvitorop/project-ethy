74db1095e5c5c027c7575f6435b932a9bf92071c
MATVII <mattor264@gmail.com>
2026-04-16 12:40:13 +0300

    Create command and handler for proposing stages

---
+++ server/Application/Handlers/ProposeStage/ProposeStageCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.ProposeStage
+{
+    public sealed record ProposeStageCommand(
+        Guid HelpRequestId,
+        Guid ChatId,
+        Guid UserId,
+        string Content
+    ) : IRequest<Result<Guid>>;
+}
