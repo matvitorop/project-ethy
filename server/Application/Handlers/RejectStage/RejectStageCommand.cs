9f1bafc5561efb79812b9f44351d5292f2d0cb3d
MATVII <mattor264@gmail.com>
2026-04-16 13:10:18 +0300

    Create command and handler for rehecting stages

---
+++ server/Application/Handlers/RejectStage/RejectStageCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.RejectStage
+{
+    public sealed record RejectStageCommand(
+        Guid StageId,
+        Guid UserId,
+        string Reason
+    ) : IRequest<Result>;
+}
