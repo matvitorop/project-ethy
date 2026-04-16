273c67f00b81e6a5af01905e0897f3601ebc6223
MATVII <mattor264@gmail.com>
2026-04-16 13:14:13 +0300

    Create query and handler for getting stages for request

---
+++ server/Application/Handlers/GetStages/GetStagesHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using server.Application.IRepositories;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.GetStages
+{
+    public sealed class GetStagesHandler
+        : IRequestHandler<GetStagesQuery, Result<IReadOnlyList<StageDto>>>
+    {
+        private readonly IStageRepository _stageRepository;
+
+        public GetStagesHandler(IStageRepository stageRepository)
+        {
+            _stageRepository = stageRepository;
+        }
+
+        public async Task<Result<IReadOnlyList<StageDto>>> Handle(
+            GetStagesQuery request, CancellationToken ct)
+        {
+            var stages = await _stageRepository
+                .GetStagesAsync(request.HelpRequestId, ct);
+
+            return Result<IReadOnlyList<StageDto>>.Success(stages);
+        }
+    }
+}
