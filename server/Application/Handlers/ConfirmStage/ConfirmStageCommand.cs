70d3ddd5a051c6d01585b8317592155ad74262f7
MATVII <mattor264@gmail.com>
2026-04-16 12:57:12 +0300

    Create command and handler for confirming stages

---
+++ server/Application/Handlers/ConfirmStage/ConfirmStageCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.ConfirmStage
+{
+    public sealed record ConfirmStageCommand(
+        Guid StageId,
+        Guid UserId
+    ) : IRequest<Result>;
+}
