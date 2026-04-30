6d7fa419c767e45c5ed298cfb0202b669c8afba1
MATVII <mattor264@gmail.com>
2026-04-30 23:22:30 +0300

    Add types and resolvers for deleting and cancelling hr

---
+++ server/Presentation/GraphQL/Types/CancelHelpRequestTypes/CancelHelpRequestPayload.cs
@@ -0,0 +1,9 @@
+﻿using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.CancelHelpRequestTypes
+{
+    public sealed record CancelHelpRequestPayload(
+        bool Success,
+        ErrorPayload? Error
+    );
+}
---
+++ server/Presentation/GraphQL/Types/SoftDeleteHelpRequestTypes/SoftDeleteHelpRequestPayloadType.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.Types;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.SoftDeleteHelpRequestTypes
+{
+    public sealed class SoftDeleteHelpRequestPayloadType
+        : ObjectGraphType<SoftDeleteHelpRequestPayload>
+    {
+        public SoftDeleteHelpRequestPayloadType()
+        {
+            Field(x => x.Success);
+            Field<ErrorPayloadType>("error")
+                .Resolve(ctx => ctx.Source.Error);
+        }
+    }
+}
