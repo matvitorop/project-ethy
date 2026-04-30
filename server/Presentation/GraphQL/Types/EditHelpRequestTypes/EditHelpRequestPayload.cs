1e57bd7f2b33b542f67f1567277d33552ca173da
MATVII <mattor264@gmail.com>
2026-04-30 21:50:47 +0300

    Implemented types and resolver for editing hr

---
+++ server/Presentation/GraphQL/Types/EditHelpRequestTypes/EditHelpRequestPayload.cs
@@ -0,0 +1,9 @@
+﻿using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.EditHelpRequestTypes
+{
+    public sealed record EditHelpRequestPayload(
+        bool Success,
+        ErrorPayload? Error
+    );
+}
---
+++ server/Presentation/GraphQL/Types/EditHelpRequestTypes/EditHelpRequestPayloadType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.EditHelpRequestTypes
+{
+    public sealed class EditHelpRequestPayloadType
+        : ObjectGraphType<EditHelpRequestPayload>
+    {
+        public EditHelpRequestPayloadType()
+        {
+            Field(x => x.Success);
+
+            Field<ErrorPayloadType>("error")
+                .Resolve(ctx => ctx.Source.Error);
+        }
+    }
+}
