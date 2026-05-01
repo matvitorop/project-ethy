82974be01573ce10b792b3cf1bfe5a996046a77d
MATVII <mattor264@gmail.com>
2026-05-01 15:12:14 +0300

    Implemented updating username, password, getting by id in user repo

--- server/Application/IRepositories/IUserRepository.cs
+++ server/Application/IRepositories/IUserRepository.cs
@@ -8,5 +8,8 @@ namespace server.Application.IRepositories
         Task<User?> GetByEmailAsync(string email);
         Task<User?> AddAsync(User user);
         Task<UserStatisticsDto?> GetUserStatisticsAsync(Guid userId, CancellationToken ct);
+        Task<User?> GetByIdAsync(Guid id, CancellationToken ct);
+        Task UpdateUsernameAsync(Guid id, string username, CancellationToken ct);
+        Task UpdatePasswordAsync(Guid id, string passwordHash, string passwordSalt, CancellationToken ct);
     }
 }
