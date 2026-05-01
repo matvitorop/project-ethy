8cf922c4a4481500e6700439651959f5f8b1584c
MATVII <mattor264@gmail.com>
2026-05-01 15:31:13 +0300

    Create appropriate types for updating, changing and getting user data

---
+++ server/Presentation/GraphQL/Types/ProfileTypes/UpdateUsernamePayload.cs
@@ -0,0 +1,9 @@
+﻿using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.ProfileTypes
+{
+    public sealed record UpdateUsernamePayload(
+        bool Success,
+        ErrorPayload? Error
+    );
+}
