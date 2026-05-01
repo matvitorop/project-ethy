bf2ff44eaba91492987a9df1376f662070613b7c
MATVII <mattor264@gmail.com>
2026-05-01 15:05:33 +0300

    Add handler, dto and query for getting profile

---
+++ server/Application/Handlers/User/GetProfile/GetProfileQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.User.GetProfile
+{
+    public sealed record GetProfileQuery(
+        Guid UserId
+    ) : IRequest<Result<ProfileDto>>;
+}
---
+++ server/Application/Handlers/User/GetProfile/ProfileDto.cs
@@ -0,0 +1,9 @@
+﻿namespace server.Application.Handlers.User.GetProfile
+{
+    public sealed record ProfileDto(
+        Guid Id,
+        string Username,
+        string Email,
+        DateTime RegisteredAtUtc
+    );
+}
