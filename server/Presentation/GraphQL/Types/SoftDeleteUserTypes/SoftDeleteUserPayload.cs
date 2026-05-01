e5bb337aedc49382b9d98983f4b1c9b67c4f8fd1
MATVII <mattor264@gmail.com>
2026-05-01 16:54:44 +0300

    New types and resolver for accounts soft delete added

---
+++ server/Presentation/GraphQL/Types/SoftDeleteUserTypes/SoftDeleteUserPayload.cs
@@ -0,0 +1,9 @@
+﻿using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.SoftDeleteUserTypes
+{
+    public sealed record SoftDeleteUserPayload(
+        bool Success,
+        ErrorPayload? Error
+    );
+}
