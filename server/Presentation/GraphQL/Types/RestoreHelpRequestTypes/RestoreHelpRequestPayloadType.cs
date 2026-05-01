65299c786cc76586374c642cea65e5a7c0df7729
MATVII <mattor264@gmail.com>
2026-05-01 14:58:47 +0300

    Implemented appropriate types and resolver for restoring hr

---
+++ server/Presentation/GraphQL/Types/RestoreHelpRequestTypes/RestoreHelpRequestPayloadType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.RestoreHelpRequestTypes
+{
+    public sealed class RestoreHelpRequestPayloadType
+        : ObjectGraphType<RestoreHelpRequestPayload>
+    {
+        public RestoreHelpRequestPayloadType()
+        {
+            Field(x => x.Success);
+
+            Field<ErrorPayloadType>("error")
+                .Resolve(ctx => ctx.Source.Error);
+        }
+    }
+}
