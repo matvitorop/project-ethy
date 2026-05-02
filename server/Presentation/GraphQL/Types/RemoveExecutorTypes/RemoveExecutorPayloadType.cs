cfe00a43b70062349d3f34165ab4c79745a420c4
MATVII <mattor264@gmail.com>
2026-05-02 16:11:41 +0300

    Add types and resolver for removing executor in inProgress hr

---
+++ server/Presentation/GraphQL/Types/RemoveExecutorTypes/RemoveExecutorPayloadType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.RemoveExecutorTypes
+{
+    public sealed class RemoveExecutorPayloadType
+        : ObjectGraphType<RemoveExecutorPayload>
+    {
+        public RemoveExecutorPayloadType()
+        {
+            Field(x => x.Success);
+
+            Field<ErrorPayloadType>("error")
+                .Resolve(ctx => ctx.Source.Error);
+        }
+    }
+}
