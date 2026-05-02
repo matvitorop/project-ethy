2fea7e08334182c65083e0d666ecb74daecf876b
MATVII <mattor264@gmail.com>
2026-05-02 13:35:00 +0300

    Create resolver and types for cancelling response

---
+++ server/Presentation/GraphQL/Types/CancelResponseTypes/CancelResponsePayloadType.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.CancelResponseTypes
+{
+    public sealed class CancelResponsePayloadType
+        : ObjectGraphType<CancelResponsePayload>
+    {
+        public CancelResponsePayloadType()
+        {
+            Field(x => x.Success);
+
+            Field<ErrorPayloadType>("error")
+                .Resolve(ctx => ctx.Source.Error);
+        }
+    }
+}
