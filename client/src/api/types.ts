30adc52cb12da8ccf3608dfed55599911359ed1b
MATVII <mattor264@gmail.com>
2026-05-03 01:55:05 +0300

    Add some type for ci confirmation

---
+++ client/src/api/types.ts
@@ -0,0 +1,28 @@
+export interface ApiError {
+    code: string
+    message: string
+}
+
+export interface AuthPayload {
+    token: string | null
+    error: ApiError | null
+}
+
+export interface LoginData {
+    auth: { login: AuthPayload }
+}
+
+export interface LoginVars {
+    email: string
+    password: string
+}
+
+export interface RegisterData {
+    auth: { register: AuthPayload }
+}
+
+export interface RegisterVars {
+    username: string
+    email: string
+    password: string
+}
