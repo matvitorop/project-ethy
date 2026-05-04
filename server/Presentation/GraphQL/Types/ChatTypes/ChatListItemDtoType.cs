d96483c36f08d7291e0d4685c56efe0b9f9c7936
MATVII <mattor264@gmail.com>
2026-05-04 14:06:58 +0300

    Add types and resolver for getting user chat

---
+++ server/Presentation/GraphQL/Types/ChatTypes/ChatListItemDtoType.cs
@@ -0,0 +1,18 @@
+﻿using GraphQL.Types;
+using server.Application.Handlers.GetMyChats;
+
+namespace server.Presentation.GraphQL.Types.ChatTypes
+{
+    public sealed class ChatListItemDtoType : ObjectGraphType<ChatListItemDto>
+    {
+        public ChatListItemDtoType()
+        {
+            Field(x => x.ChatId, type: typeof(IdGraphType));
+            Field(x => x.HelpRequestId, type: typeof(GuidGraphType));
+            Field(x => x.HelpRequestTitle);
+            Field(x => x.OwnerId, type: typeof(GuidGraphType));
+            Field(x => x.AssigneeId, type: typeof(GuidGraphType));
+            Field(x => x.CreatedAtUtc);
+        }
+    }
+}
