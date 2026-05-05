5a48f60c7b1bbc7d666499dbf0087e7b5caceacf
MATVII <mattor264@gmail.com>
2026-05-05 18:10:20 +0300

    Create handlers for getting reviews and leaving complaints, add repo for complaints functionality

---
+++ server/Application/Handlers/UserHandlers/GetUserReviews/GetUserReviewsHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using server.Application.IRepositories;
+using server.Domain.Primitives;
+
+namespace server.Application.Handlers.UserHandlers.GetUserReviews
+{
+    public sealed class GetUserReviewsHandler
+    : IRequestHandler<GetUserReviewsQuery, Result<IEnumerable<UserReviewDto>>>
+    {
+        private readonly IReviewRepository _reviewRepository;
+
+        public GetUserReviewsHandler(IReviewRepository reviewRepository)
+        {
+            _reviewRepository = reviewRepository;
+        }
+
+        public async Task<Result<IEnumerable<UserReviewDto>>> Handle(
+            GetUserReviewsQuery request,
+            CancellationToken ct)
+        {
+            var reviews = await _reviewRepository
+                .GetByTargetUserAsync(request.TargetUserId, ct);
+
+            return Result<IEnumerable<UserReviewDto>>.Success(reviews);
+        }
+    }
+
+}
---
+++ server/Application/IRepositories/IComplaintRepository.cs
@@ -0,0 +1,10 @@
+﻿using server.Domain.ReviewAndComplaints;
+
+namespace server.Application.IRepositories
+{
+    public interface IComplaintRepository
+    {
+        Task AddAsync(UserComplaint complaint, CancellationToken ct);
+    }
+
+}
