8463558bc026409bc314cb8f2d91fb9ed711fcbd
MATVII <mattor264@gmail.com>
2026-05-05 17:45:53 +0300

    Add migrations for trust and review system

---
+++ server/Database/Migrations/017_Create_Userviews.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE UserReviews (
+    Id              UNIQUEIDENTIFIER NOT NULL CONSTRAINT PK_UserReviews PRIMARY KEY,
+    HelpRequestId   UNIQUEIDENTIFIER NOT NULL REFERENCES HelpRequests(Id),
+    ReviewerUserId  UNIQUEIDENTIFIER NOT NULL REFERENCES Users(Id),
+    TargetUserId    UNIQUEIDENTIFIER NOT NULL REFERENCES Users(Id),
+    IsPositive      BIT              NOT NULL,
+    Comment         NVARCHAR(1000)   NULL,
+    CreatedAtUtc    DATETIME2        NOT NULL,
+
+    CONSTRAINT UQ_UserReviews_HelpRequest_Reviewer
+        UNIQUE (HelpRequestId, ReviewerUserId)
+);
