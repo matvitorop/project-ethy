8003a0eb7a1fa03f1de38211e94766ff480dd351
MATVII <mattor264@gmail.com>
2026-05-06 17:41:26 +0300

    Create repo interfaces for users block history, verify email and vol. applications

---
+++ server/Application/IRepositories/IVolunteerApplicationRepository.cs
@@ -0,0 +1,13 @@
+﻿using server.Domain.UserAndVolunteer;
+
+namespace server.Application.IRepositories
+{
+    public interface IVolunteerApplicationRepository
+    {
+        Task AddAsync(VolunteerApplication application, CancellationToken ct);
+        Task<VolunteerApplication?> GetByIdAsync(Guid id, CancellationToken ct);
+        Task<VolunteerApplication?> GetPendingByUserIdAsync(Guid userId, CancellationToken ct);
+        Task<List<VolunteerApplicationDto>> GetAllAsync(int? status, CancellationToken ct);
+        Task UpdateAsync(VolunteerApplication application, CancellationToken ct);
+    }
+}
