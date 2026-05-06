5c834ab540fa5a6f81628abbe76a2e9c794e9f55
MATVII <mattor264@gmail.com>
2026-05-06 16:29:45 +0300

    Add migrations for blocking fucntionality, email verification, vol applications and user block history

---
+++ server/Database/Migrations/019_Add_Block_And_Hidden_Fields_And_IsRosolved_In_UserComplaint.sql
@@ -0,0 +1,9 @@
+﻿ALTER TABLE Users
+    ADD BlockedUntilUtc DATETIME2 NULL,
+        BlockReason NVARCHAR(500) NULL,
+        LastVolunteerApplicationAtUtc DATETIME2 NULL;
+
+ALTER TABLE HelpRequests
+    ADD IsHidden BIT NOT NULL DEFAULT 0;
+
+ALTER TABLE UserComplaints ADD IsResolved BIT NOT NULL DEFAULT 0;
