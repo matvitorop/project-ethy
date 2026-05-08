3a764408167283b5ec11c1ce7c996498b808ff33
MATVII <mattor264@gmail.com>
2026-05-08 17:27:11 +0300

    Add types and queries for getting stats, modified changing status handler

---
+++ server/Presentation/GraphQL/Types/StatsTypes/TopVolunteerByReviewsDtoType.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL.Types;
+using server.Application.Handlers.StatisticsHandlers.GetTopVolunteers;
+
+namespace server.Presentation.GraphQL.Types.StatsTypes
+{
+    public class TopVolunteerByReviewsDtoType : ObjectGraphType<TopVolunteerByReviewsDto>
+    {
+        public TopVolunteerByReviewsDtoType()
+        {
+            Field(x => x.UserId, type: typeof(IdGraphType));
+            Field(x => x.Username);
+            Field(x => x.PositiveReviews);
+        }
+    }
+}
---
+++ server/Presentation/GraphQL/Types/StatsTypes/TopVolunteersPayload.cs
@@ -0,0 +1,7 @@
+﻿using server.Application.Handlers.StatisticsHandlers.GetTopVolunteers;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.StatsTypes
+{
+    public record TopVolunteersPayload(TopVolunteersDto? Data, ErrorPayload? Error);
+}
