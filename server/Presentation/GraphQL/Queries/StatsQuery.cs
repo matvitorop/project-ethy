1985a75ed37eb2d5faf668ce57492be103761087
MATVII <mattor264@gmail.com>
2026-05-08 20:41:20 +0300

    Create handler and resolver for admin statistic

--- server/Presentation/GraphQL/Queries/StatsQuery.cs
+++ server/Presentation/GraphQL/Queries/StatsQuery.cs
@@ -1,6 +1,7 @@
 ﻿using GraphQL;
 using GraphQL.Types;
 using MediatR;
+using server.Application.Handlers.StatisticsHandlers.GetAdminAnalytics;
 using server.Application.Handlers.StatisticsHandlers.GetMonthlyActivity;
 using server.Application.Handlers.StatisticsHandlers.GetPlatformStats;
 using server.Application.Handlers.StatisticsHandlers.GetTopVolunteers;
@@ -34,6 +35,14 @@ namespace server.Presentation.GraphQL.Queries
                 var limit = ctx.GetArgument<int?>("limit") ?? 5;
                 var r = await mediator.Send(new GetTopVolunteersQuery(limit));
                 return r.ToPayload((val, err) => new TopVolunteersPayload(val, err));
+            });
+
+            Field<AdminAnalyticsPayloadType>("adminAnalytics")
+            .Authorize().AuthorizeWithRoles("Admin")
+            .ResolveAsync(async ctx =>
+            {
+                var r = await mediator.Send(new GetAdminAnalyticsQuery());
+                return r.ToPayload((val, err) => new AdminAnalyticsPayload(val, err));
             });
         }
     }
---
+++ server/Presentation/GraphQL/Types/StatsTypes/AdminAnalyticsPayload.cs
@@ -0,0 +1,7 @@
+﻿using server.Application.Handlers.StatisticsHandlers.GetAdminAnalytics;
+using server.Presentation.GraphQL.Types.ErrorTypes;
+
+namespace server.Presentation.GraphQL.Types.StatsTypes
+{
+    public record AdminAnalyticsPayload(AdminAnalyticsDto? Data, ErrorPayload? Error);
+}
